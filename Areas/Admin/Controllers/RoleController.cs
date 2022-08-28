88da554130af2f4151aaea285d88d85c02f3f09d
pranit29 <70082270+pranit29@users.noreply.github.com>
2022-08-28 10:54:13 +0530

    Add files via upload

---
+++ Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,60 @@
+﻿using EShop.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityRole> _roleManager;
+        UserManager<IdentityUser> _userManager;
+        ApplicationDbContext _db;
+        public RoleController(RoleManager<IdentityRole> roleManager, ApplicationDbContext db, UserManager<IdentityUser> userManager)
+        {
+            _roleManager = roleManager;
+            _db = db;
+
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            ViewBag.Roles = roles;
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            IdentityRole role = new IdentityRole();
+            role.Name = name;
+            var isExist = await _roleManager.RoleExistsAsync(role.Name);
+            if (isExist)
+            {
+                ViewBag.mgs = "This role is aldeady exist";
+                ViewBag.name = name;
+                return View();
+            }
+            var result = await _roleManager.CreateAsync(role);
+            if (result.Succeeded)
+            {
+                TempData["save"] = "Role has been saved successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+
+    }
+}
